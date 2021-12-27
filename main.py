57fc426383e8f27c3ccbc55af1be24fe3ad6dcf6
John Adams Jr <john.c.adams32@gmail.com>
2021-12-27 13:54:01 -0700

    Adding the logo file

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# For day 16 of 100 days of code, the task is to re-create the same coffee machine from day 15. Only this time using,
+# object-oriented programming. Main.py is the only file I wrote my own code in, feel free to poke around and use this
+# amazing coffee machine.
+
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from logo import logo
+
+print(logo)
