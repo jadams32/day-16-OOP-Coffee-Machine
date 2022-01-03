8c474f948aaf694fbc49a079157c25f2f053b2bd
John Adams Jr <john.c.adams32@gmail.com>
2022-01-02 22:00:09 -0700

    Machine complete, still needs comments.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # object-oriented programming. Main.py is the only file I wrote my own code in, feel free to poke around and use this
 # amazing coffee machine.
 
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 from logo import logo
@@ -10,7 +10,6 @@ from logo import logo
 print(logo)
 
 menu = Menu()
-# menu_item = MenuItem()
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 is_on = True
@@ -18,14 +17,16 @@ is_on = True
 while is_on:
 
     initial_selection = input(f"What would you like today? {menu.get_items()}:")
+
     if initial_selection == "report":
         coffee_maker.report()
         money_machine.report()
+
     elif initial_selection == "off":
         is_on = False
+
     else:
         drink_selection = menu.find_drink(initial_selection)
         if coffee_maker.is_resource_sufficient(drink_selection):
-            money_machine.process_coins()
-            if money_machine.make_payment(drink_selection):
-                coffee_maker.make_coffee()
+            if money_machine.make_payment(drink_selection.cost):
+                coffee_maker.make_coffee(drink_selection)
